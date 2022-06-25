a2667e319078a6e58860dcf845994ef74d5a8c98
Islombek <mislombek1512@gmail.com>
2022-06-25 08:18:38 +0500

    add contact model

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function getcontact(Request $req)
+    {
+        try {
+            $data =Contact::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
 }
