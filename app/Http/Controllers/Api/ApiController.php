23ed4704d39778d4ce6d2e340bec83fb24fc646f
Islombek <mislombek1512@gmail.com>
2022-06-28 07:15:24 +0500

    Mustafo

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,7 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bachelor;
+use App\Models\Bussin;
+use App\Models\Congratulation;
 use App\Models\Contact;
+use App\Models\Fact;
+use App\Models\Facult;
+use App\Models\Graduat;
+use App\Models\Nam;
+use App\Models\Rectorat;
+use App\Models\Student;
+use App\Models\Studentmen;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
@@ -25,4 +35,184 @@ class ApiController extends Controller
                 ]);
         }
     }
+    public function getbussins(Request $req)
+    {
+        try {
+            $data =Bussin::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+
+    public function getstudentmens(Request $req)
+    {
+        try {
+            $data =Studentmen::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+
+    public function getstudents(Request $req)
+    {
+        try {
+            $data =Student::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+
+    public function getbachelors(Request $req)
+    {
+        try {
+            $data =Bachelor::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+
+    public function getgraduats(Request $req)
+    {
+        try {
+            $data =Graduat::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+
+    public function getfacults(Request $req)
+    {
+        try {
+            $data =Facult::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+
+    public function getnams(Request $req)
+    {
+        try {
+            $data =Nam::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+    public function getfacts(Request $req)
+    {
+        try {
+            $data =Fact::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+
+    public function getcongratulations(Request $req)
+    {
+        try {
+            $data =Congratulation::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
+
+    public function getrectorats(Request $req)
+    {
+        try {
+            $data =Rectorat::all();
+            return
+                response()->json([
+                    'ok' => true,
+                    'data' => $data,
+                ]);
+        } catch (\Exception $e) {
+            return
+                response()->json([
+                    'ok' => false,
+                    'msg' => $e->getMessage(),
+                ]);
+        }
+    }
 }
+
+
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/contacts', [ApiController::class, 'getcontact']);
+Route::get('/bussins', [ApiController::class, 'getbussins']);
+Route::get('/studentmens', [ApiController::class, 'getstudentmens']);
+Route::get('/students', [ApiController::class, 'getstudents']);
+Route::get('/bachelors', [ApiController::class, 'getbachelors']);
+Route::get('/graduats', [ApiController::class, 'getgraduats']);
+Route::get('/nams', [ApiController::class, 'getnams']);
+Route::get('/facts', [ApiController::class, 'getfacts']);
+Route::get('/congratulations', [ApiController::class, 'getcongratulations']);
+Route::get('/rectorats', [ApiController::class, 'getrectorats']);
